a15565972eaf3617e24cfa78e8242c3b20dbaad0
Jacob Menashi <jbmenashi@gmail.com>
2020-05-18 22:24:23 -0500

    Ready to gather data

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
-import Players from './components/Players';
 
 class App extends Component {
   render() {
@@ -10,7 +9,6 @@ class App extends Component {
       <div>
         <Switch>
           <Route exact path='/' component={Home}/>
-          <Route path='/players' component={Players}/>
         </Switch>
       </div>
     )
